db8d55646a4f1c61a12ceede27553cc4d6029600
teaa <91749021+teabojovic@users.noreply.github.com>
2022-01-16 20:33:07 +0100

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -3,6 +3,8 @@ from tkinter import Entry, StringVar, Label
 from tkinter import messagebox
 from bs4 import BeautifulSoup
 import requests
+from datetime import datetime
+import time
 
 def get_numbers():
     '''Scrapes numbers from sudoku site'''
@@ -14,8 +16,6 @@ def get_numbers():
     page_html = BeautifulSoup(response.text, 'html.parser')
     table_container = page_html.find("table", {"id": "puzzle_grid"})
     list = table_container.find_all("input")
-    solution1 = page_html.find("input", {"name": "cheat"})
-    solution = solution1.get("value")
 def matrix9x9():
     '''makes matrix 9x9 with zeros'''
     global matrix
@@ -33,43 +33,52 @@ def make_matrix9x9():
         for j in range(9):
             a.append(0)
         solution_matrix.append(a)
-def solution_matrixx():
-    make_matrix9x9()
+def make_entry_matrix():
+    global entry_matrix
+    entry_matrix = []
     for i in range(9):
+        a =[]
         for j in range(9):
-            solution_matrix[i][j] = solution[9*i+j]
+            a.append(0)
+        entry_matrix.append(a)
 def num_matrix():
     '''now we put numbers that we get using get_numbers() in matrix 9x9'''
-    matrix9x9()
-    get_numbers()
-    solution_matrixx()
     for k in range(9):
         for r in range(9):
             if list[9*k+r].get('value') is not None:
-                matrix[k][r] = list[9*k+r].get('value')
+               matrix[k][r] = list[9*k+r].get('value')
+               solution_matrix[k][r] = list[9*k+r].get('value')
 def callback(var):
     '''puts new value(from entry) in matrix and checks if the matrix is ​​filled'''
     temp = var.__str__().replace('PY_VAR', '')
     temp = int(temp)
     matrix[temp//9][temp%9] = var.get()
 def checkbutton_callback():
+    #function called, check button
     k = 0
     for i in range(9):
         for j in range(9):
+            matrix[i][j] = int(matrix[i][j])
             if matrix[i][j] == solution_matrix[i][j]:
                 k = k + 1
     if k == 81:
-        messagebox.showinfo("End","Bravoo!!!!")
+        messagebox.showinfo("Bravoo!!!!", datetime.now() - start_time)
     else:
-        messagebox.showinfo("End","Try again")
-
+        messagebox.showinfo("Try again", datetime.now() - start_time)
 def regular1(P):
     '''checks the number in the entry(0 < P < 10...)'''
     num = len(P) < 2 and (P.isdigit() or P == "")
     return num #returns num, after checking
+def solvebutton_callback():
+    #function called, solve button
+    for i in range(9):
+        for j in range(9):
+            if entry_matrix[i][j] != 0:
+                entry_matrix[i][j].delete(0, "end")
+                entry_matrix[i][j].insert(0, solution_matrix[i][j])
 def make_grid():
     '''function makes sudoku table with buttons and entries'''
-    num_matrix()
+    make_entry_matrix()
     regular = root.register(regular1)
     for row in range(9):
         for column in range(9):
@@ -80,6 +89,7 @@ def make_grid():
                 entryb.grid(row = row, column = column, padx = 1, pady = 1, ipady = 10)
                 entryb.config(validate="key", validatecommand=(regular, "%P"))
                 entryb.config(cursor = "plus", font = ('Centry 10'))
+                entry_matrix[row][column] = entryb
             else:
                 vaar = StringVar()
                 vaar.set(matrix[row][column])
@@ -87,15 +97,63 @@ def make_grid():
                 #makes label and puts number(from matrix) in it
                 label.grid(row = row, column = column, padx = 1, pady = 1, ipady = 10)
                 label.config(cursor = "plus", font = ('Centry 10'))
-    b = tk.Button(root, text ="Check", command = checkbutton_callback)
-    b.grid(row = 9, padx = 1, pady = 1, ipady = 10)
+    b = tk.Button(root, text = "Check", command = checkbutton_callback)
+    b.grid(row = 9, column = 0, padx = 1, pady = 1, ipady = 20, columnspan = 4, ipadx = 70)
+    b1 = tk.Button(root, text = "Solve", command = solvebutton_callback)
+    b1.grid(row = 9, column = 4, padx = 1, pady = 1, ipady = 20, columnspan = 5, ipadx = 95)
+
+def ok_num(solution_matrix, row, col, num):
+    
+    for i in range(9):
+        if solution_matrix[row][i] == num:
+            return False
+    for i in range(9):
+        if solution_matrix[i][col] == num:
+            return False
+
+    start_row = row - row % 3
+    start_col = col - col % 3
+
+    for i in range(3):
+        for j in range(3):
+            if solution_matrix[start_row+i][start_col+j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(solution_matrix, row, col):
+    if row == 8 and col == 9:
+        return True
+    
+    if col == 9:
+       row += 1
+       col = 0
+    solution_matrix[row][col] = int(solution_matrix[row][col])
+    if solution_matrix[row][col] > 0:
+        return solve_sudoku(solution_matrix, row, col+1)
+
+    for num in range(1, 10):
+        if ok_num(solution_matrix, row, col, num):
+            solution_matrix[row][col] = num
+            if solve_sudoku(solution_matrix, row, col+1):
+                return True
+            solution_matrix[row][col] = 0
+    return False
 def sudoku_gui():
-    '''function makes root and mainloop and at the end print new matrix'''
+    '''function makes root and print new matrix'''
     global root
+    global start_time
     root = tk.Tk()
     root.title("S U D O K U   G A M E")
     root.configure(bg="black")
     make_grid()
+    start_time = datetime.now()
     root.mainloop()
+get_numbers()
+matrix9x9()
+make_matrix9x9()
+num_matrix() 
+solve_sudoku(solution_matrix, 0, 0)
 sudoku_gui()
+
 
